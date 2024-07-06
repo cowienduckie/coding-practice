ae1b2e88b48095f5cafdf5da55e8e6b2ea412ec3
Minh Tran <minhtran.2404@outlook.com>
2024-07-06 14:56:42 +0700

    initial commit

---
+++ LeetCode/DailyProblems/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DailyProblems;
+
+internal static class Program
+{
+    internal static void Main(string[] _)
+    {
+        var solution = new Solutions.Problem1509.Solution();
+
+        var ans = solution.MinDifference(new[] { 9,48,92,48,81,31 });
+
+        Console.WriteLine(ans);
+    }
+}
