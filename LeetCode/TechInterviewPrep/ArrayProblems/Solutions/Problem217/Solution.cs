dbe8c2afe4954fe8a261af8577d22aa002d9afb2
Minh Tran <minhtran.2404@outlook.com>
2024-07-25 01:00:42 +0700

    update Array related problems

---
+++ LeetCode/TechInterviewPrep/ArrayProblems/Solutions/Problem217/Solution.cs
@@ -0,0 +1,16 @@
+namespace ArrayProblems.Solutions.Problem217;
+
+public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        if(nums.Length == 1)
+        {
+            return false;
+        }
+
+        var set = new HashSet<int>();
+
+        return nums.Any(num => !set.Add(num));
+    }
+}
